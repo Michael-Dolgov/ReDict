1b8350741260dd295de14f641707117f42ee6c09
MIchael Dolgov <michael.dlgov@gmail.com>
2025-09-03 14:24:41 +0300

    test

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+    //load conf
+    //open listener sock
+    //inject listener sock to async main event loop
+    //log all event which appears in event loop
+    //TODO: async event loop
     return EXIT_SUCCESS;
 }
